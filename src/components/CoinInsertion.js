8007320593c2d4885cc5e3e8e960b5b77e5e327d
Peter Matov <pmatov@gmail.com>
2023-05-24 00:54:32 +0300

    [C] Adding images to the Card component

--- src/components/CoinInsertion.js
+++ src/components/CoinInsertion.js
@@ -1,14 +1,30 @@
-import { Button, Typography } from "@mui/material";
+import { Button, Typography, Grid } from "@mui/material";
+import { styled } from "@mui/system";
+
+const CoinButton = styled(Button)(({ theme }) => ({
+  borderRadius: "50%",
+  width: "50px",
+  height: "50px",
+  backgroundColor: "#FFD700",
+  color: "black",
+  margin: "5px",
+  "&:hover": {
+    backgroundColor: "#C0C0C0",
+    transform: "scale(1.1)",
+  },
+}));
 
 const CoinInsertion = ({ onInsertCoin }) => {
   return (
     <div>
       <Typography variant="h6">Insert Coin:</Typography>
-      {[0.01, 0.05, 0.1, 0.25, 0.5, 1].map((coin, index) => (
-        <Button key={index} onClick={() => onInsertCoin(coin)}>
-          ${coin}
-        </Button>
-      ))}
+      <Grid container spacing={2}>
+        {[0.01, 0.05, 0.1, 0.25, 0.5, 1].map((coin, index) => (
+          <Grid item xs={6} key={index}>
+            <CoinButton onClick={() => onInsertCoin(coin)}>${coin}</CoinButton>
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
 };
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,20 +1,32 @@
-import { Button, Card, CardContent, Typography } from "@mui/material";
+import { Card, CardContent, Typography, CardMedia } from "@mui/material";
+import { styled } from "@mui/system";
+
+// Styled Card for extra customization.
+const StyledCard = styled(Card)(({ theme }) => ({
+  width: "100%",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  cursor: "pointer",
+  "&:hover": {
+    transform: "scale(1.05)",
+  },
+}));
 
 const Product = ({ product, onSelectProduct }) => {
   return (
-    <Card style={{ width: "100%" }}>
+    <StyledCard onClick={() => onSelectProduct(product)}>
+      <CardMedia
+        component="img"
+        height="140"
+        image={product.image}
+        alt={product.name}
+      />
       <CardContent>
         <Typography variant="h5">{product.name}</Typography>
-        <Typography variant="body2">${product.price}</Typography>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => onSelectProduct(product)}
-        >
-          Buy
-        </Button>
+        <Typography variant="h6">${product.price.toFixed(2)}</Typography>
       </CardContent>
-    </Card>
+    </StyledCard>
   );
 };
 
