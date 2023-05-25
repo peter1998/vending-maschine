debbd632ce0cd4568d3b11913133232c02ea474a
Peter Matov <pmatov@gmail.com>
2023-05-26 00:36:43 +0300

    [C] initialise background - image

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import VendingMachine from "./components/VendingMachine ";
+import "./App.css";
 
 const App = () => {
-  return <VendingMachine />;
+  return (
+    <div className="app">
+      <div className="parent-container">
+        <VendingMachine />
+      </div>
+    </div>
+  );
 };
 
 export default App;
