0b67ea3a18ce6f57a4464fabc442d87877997cb9
Peter Matov <pmatov@gmail.com>
2023-05-26 00:52:53 +0300

    [C] Adding box shadow property

--- src/components/VendingMachineCard.js
+++ src/components/VendingMachineCard.js
@@ -6,7 +6,8 @@ const StyledCard = styled(Card)(({ theme }) => ({
   margin: "2rem auto",
   borderRadius: "25px",
   background: "linear-gradient(to right, #11998e, #38ef7d)",
-  boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+  boxShadow:
+    "0 3px 5px 2px rgba(255, 105, 135, .3), 20px 20px 50px rgba(0, 0, 0, 0.5)",
   color: "white",
 }));
 
